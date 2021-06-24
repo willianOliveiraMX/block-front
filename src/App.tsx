a89e24dd2d0bf99295d778debee6b876f444b8bb
@willianvx <willian.alves@bloom.com>
2021-06-23 21:51:17 -0300

    test 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const  App = () =>{
+const App = () => {
   console.log('batata');
   return (
     <div>
       block front aqui
     </div>
   );
-}
+};
 
 export default App;
